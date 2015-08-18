ef6e788f3a1305688957a70e451101ba7cd1fffc
Axure <axurez@vip.qq.com>
2015-08-19 01:30:36 +0800

    Made the template to work with Java 1.8.

---
+++ src/main/java/com/springapp/mvc/PublicController.java
@@ -0,0 +1,21 @@
+package com.springapp.mvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/public")
+public class PublicController {
+    @RequestMapping(method = RequestMethod.GET)
+    public String showPublic(ModelMap model) {
+        model.addAttribute("body", "This is the public body.");
+        return "public";
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public String addPublic(ModelMap model) {
+        return "public";
+    }
+}
